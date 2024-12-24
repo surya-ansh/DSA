64e367335397fb937c1abc760dcb0d1cc55b8898
surya-ansh <suryansh.singh7011609038@gmail.com>
2024-12-24 12:22:30 +0530

    first message

---
+++ Array.cpp
@@ -0,0 +1,19 @@
+// in array questions where we have to move elements->>>>>take first element and do the operations   
+// int index = 0;
+
+// 283. Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for(int i = index; i<nums.size(); i++){
+            nums[i] = 0;
+        }
+    }
+};
